8e95323d7a5d07920614d707af8a3f8e8080a112
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-11 22:41:23 +0600

    config setup

--- journal-app/nuxt.config.ts
+++ journal-app/nuxt.config.ts
@@ -3,5 +3,13 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: false }
+  devtools: { enabled: false },
+  css: ['pico-css/css/pico.min.css'],
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })

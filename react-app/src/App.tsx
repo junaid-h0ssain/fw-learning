b708b921e0850db5673ffd72d7f681f033dcef18
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-14 03:01:38 +0600

    react app init

---
+++ react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl font-bold text-green-600">
+          Hello World
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App

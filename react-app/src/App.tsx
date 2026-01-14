5360296d62a2ba9c3719b9a2557b60d697f77972
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-14 19:30:53 +0600

    layer made

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,28 +2,36 @@ import { useState } from 'react'
 
 import './App.css'
 
+function NumberButton({bValue}:{bValue:number}) {
+  return (
+    <button className='px-6 py-2 bg-gray-400 hover:bg-gray-500 text-white'> {bValue} </button>
+  )
+}
+
 function App() {
 
-  const name  = 'Jonh Doe';
-  let age = 30;
-  let salary = 50000;
+  const [value, setValue] = useState(0)
+
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <p>Name: {name}</p>
-        <p>Age: {age}</p>
-        <p>Salary: ${salary}</p>
+      <div className="app-container">
+        <div className='align-center justify-center flex flex-col gap-4'>{value}</div>
+        <NumberButton bValue={1} />
+        <NumberButton bValue={2} />
+        <NumberButton bValue={3} /><br />
+        <NumberButton bValue={4} />
+        <NumberButton bValue={5} />
+        <NumberButton bValue={6} /><br />
+        <NumberButton bValue={7} />
+        <NumberButton bValue={8} />
+        <NumberButton bValue={9} />
+
         <br />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-          onClick={() => {
-            const [salary, setSalary] = useState(50000);
-          }}>
-          Salary Increase
-        </button>
+        <button className='px-6 py-2 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg transition-colors' 
+        onClick={() => setValue(value + 1)}>Add</button>
+        <button className='px-6 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-lg transition-colors' 
+        onClick={() => setValue(value - 1)}>Subtract</button>
       </div>
     </>
   )

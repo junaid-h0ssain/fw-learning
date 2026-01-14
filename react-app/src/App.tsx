970803623b29d3902049ae8888e8180f5cddf947
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-01-14 19:06:09 +0600

    first test

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,12 +4,26 @@ import './App.css'
 
 function App() {
 
+  const name  = 'Jonh Doe';
+  let age = 30;
+  let salary = 50000;
+
   return (
     <>
       <div>
-        <h1 className="text-4xl font-bold text-green-600">
-          Hello World
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
         </h1>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Salary: ${salary}</p>
+        <br />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
+          onClick={() => {
+            const [salary, setSalary] = useState(50000);
+          }}>
+          Salary Increase
+        </button>
       </div>
     </>
   )
